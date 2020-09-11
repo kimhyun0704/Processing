60740a7a85254f00e00a93c21a4e58ea4ec014ec
kimhyun0704 <magiccastlehk@gmail.com>
2020-09-11 14:43:12 +0900

    10408 김현(9/11)
    10408 김현(9/11)

---
+++ sketch_200911c/sketch_200911c.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(500, 500);
+  fill("white");
+  ellipse(250, 250, 500, 500);
+  fill("black");
+  ellipse(250, 250, 400, 400);
+  fill("blue");
+  ellipse(250, 250, 300, 300);
+  fill("red");
+  ellipse(250, 250, 200, 200);
+  fill("yellow");
+  ellipse(250, 250, 100, 100);
+}
+
+
+function draw() {
+
+}
