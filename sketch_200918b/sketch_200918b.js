44ed6ffb1b8867b366461a5719fb7132bdafea6a
kimhyun0704 <magiccastlehk@gmail.com>
2020-09-18 14:41:02 +0900

    9/18 과제

--- sketch_200918b/sketch_200918b.js
+++ sketch_200918b/sketch_200918b.js
@@ -1,9 +1,9 @@
  function setup() {
   createCanvas(500, 500);
-  for(let i = 0; i<500; i += 10){
+  for(let i = 0; i<500; i += 15){
     line(i, 0, i, 500);
   }
-  for(let i = 0; i<500; i += 10){
+  for(let i = 0; i<500; i += 15){
     line(0, i, 500, i);
   }
 }
